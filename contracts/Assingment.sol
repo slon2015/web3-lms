c7c3420a5daf48a43139e5e10d795e00c7fb5135
Serafim <serafimgerasimov44@gmail.com>
2023-04-14 12:26:47 +0600

    feat: state channeled flow

--- contracts/Assingment.sol
+++ contracts/Assingment.sol
@@ -1,79 +1,153 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4 <0.9.0;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./Course.sol";
 
 contract Assignment {
-    struct Application {
-        string content; // IPFS hash of the application content
-        uint256 date; // timestamp of the application date
-        bool accepted; // whether the application is accepted or not
-        address teacher; // address of the teacher who accepted the application
-    }
-
     address public courseAddress; // address of the course contract that created this assignment
     string public ipfsHash; // IPFS hash of the assignment content
     uint256 public deadline; // timestamp of the assignment deadline
-    mapping(address => Application) public applications; // mapping of student addresses to their applications
 
-    event NewApplication(address indexed student, string content, uint256 date);
-    event ApplicationAccepted(address indexed student, address indexed teacher, uint256 date);
-    event ApplicationRejected(address indexed student, uint256 date);
-
-    constructor(address _courseAddress, string memory _ipfsHash, uint256 _deadline) {
+    constructor(
+        address _courseAddress,
+        string memory _ipfsHash,
+        uint256 _deadline
+    ) {
         courseAddress = _courseAddress;
         ipfsHash = _ipfsHash;
         deadline = _deadline;
     }
 
-    function submitApplication(string memory _content) public {
-        require(block.timestamp <= deadline, "Application deadline has passed");
-        require(Course(courseAddress).isEnrolled(msg.sender), "Student is not enrolled in the course");
-        require(!Course(courseAddress).hasPassed(msg.sender), "Student has already passed the course");
-
-        Application storage application = applications[msg.sender];
-        require(bytes(application.content).length == 0, "Student has already applied");
-
-        application.content = _content;
-        application.date = block.timestamp;
-
-        emit NewApplication(msg.sender, _content, block.timestamp);
+    function submitApplicationEncodedState(
+        string memory contentIpfsHash,
+        uint256 applicationTimestamp,
+        address student
+    ) internal view returns (bytes memory) {
+        return
+            abi.encodePacked(
+                courseAddress,
+                ipfsHash,
+                deadline,
+                student,
+                contentIpfsHash,
+                applicationTimestamp
+            );
     }
 
-    function acceptApplication(address _student) public {
-        require(msg.sender == Course(courseAddress).teacher(), "Only the course teacher can accept applications");
-
-        Application storage application = applications[_student];
-        require(bytes(application.content).length != 0, "Student has not applied");
-        require(!application.accepted, "Student's application has already been accepted");
-
-        application.accepted = true;
-        application.teacher = msg.sender;
-
-        emit ApplicationAccepted(_student, msg.sender, block.timestamp);
+    function submitApplication(
+        string memory contentIpfsHash,
+        uint256 applicationTimestamp,
+        address student
+    ) public view returns (bytes32) {
+        return
+            ECDSA.toEthSignedMessageHash(
+                submitApplicationEncodedState(
+                    contentIpfsHash,
+                    applicationTimestamp,
+                    student
+                )
+            );
     }
 
-    function rejectApplication(address _student) public {
-        require(msg.sender == Course(courseAddress).teacher(), "Only the course teacher can reject applications");
-
-        Application storage application = applications[_student];
-        require(bytes(application.content).length != 0, "Student has not applied");
-        require(!application.accepted, "Student's application has already been accepted");
-
-        delete applications[_student];
-
-        emit ApplicationRejected(_student, block.timestamp);
+    function rateApplicationEncodedState(
+        string memory contentIpfsHash,
+        uint256 applicationTimestamp,
+        address student,
+        address teacher,
+        uint256 rating
+    ) internal view returns (bytes memory) {
+        return
+            abi.encodePacked(
+                keccak256(
+                    submitApplicationEncodedState(
+                        contentIpfsHash,
+                        applicationTimestamp,
+                        student
+                    )
+                ),
+                teacher,
+                rating
+            );
     }
 
-    function rate(address _student, uint256 _rating) public {
-        require(msg.sender == Course(courseAddress).teacher(), "Only the course teacher can rate solutions");
+    function rateApplicationState(
+        string memory contentIpfsHash,
+        uint256 applicationTimestamp,
+        address student,
+        address teacher,
+        uint256 rating
+    ) public view returns (bytes32) {
+        return
+            ECDSA.toEthSignedMessageHash(
+                rateApplicationEncodedState(
+                    contentIpfsHash,
+                    applicationTimestamp,
+                    student,
+                    teacher,
+                    rating
+                )
+            );
+    }
 
-        // Ensure the student has submitted an application and it has been accepted
-        Application storage application = applications[_student];
-        require(bytes(application.content).length != 0, "Student has not submitted an application");
-        require(application.accepted, "Student's application has not been accepted");
+    event ApplicationPassed(
+        address indexed student,
+        address indexed teacher,
+        string assignmentTextIpfs,
+        string applicationTextIpfs,
+        uint256 deadlineTimestamp,
+        uint256 applicationTimestamp,
+        uint256 rating
+    );
 
-        // Store the rating for the student
-        Course(courseAddress).setRating(_student, _rating);
+    function saveAplicationRating(
+        string memory contentIpfsHash,
+        uint256 applicationTimestamp,
+        address student,
+        address teacher,
+        uint256 rating,
+        bytes calldata studentSignature,
+        bytes calldata teacherSignature
+    ) public {
+        require(
+            deadline >= applicationTimestamp,
+            "Assignment deadline has passed"
+        );
+        require(
+            Course(courseAddress).isEnrolled(student),
+            "Student is not enrolled in the course"
+        );
+        require(rating >= 0 && rating <= 10, "Rationg should be from 0 to 10");
+        require(
+            Course(courseAddress).teacher() == teacher,
+            "Not authorized teacher"
+        );
+
+        bytes32 messageHash = rateApplicationState(
+            contentIpfsHash,
+            applicationTimestamp,
+            student,
+            teacher,
+            rating
+        );
+
+        require(
+            ECDSA.recover(messageHash, studentSignature) == student,
+            "Invalid student signature"
+        );
+        require(
+            ECDSA.recover(messageHash, teacherSignature) == teacher,
+            "Invalid teacher signature"
+        );
+
+        emit ApplicationPassed(
+            student,
+            teacher,
+            ipfsHash,
+            contentIpfsHash,
+            deadline,
+            applicationTimestamp,
+            rating
+        );
     }
 }
